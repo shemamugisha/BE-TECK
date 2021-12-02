fb18ce84a3c8afc88643805651fb2806d65f7447
shema <azertshema@gmail.com>
2021-12-02 22:55:27 +0200

    rf(product): removig protection from the route

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,7 +4,7 @@ import protect from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.route('/save').post(protect, productController.create);
+router.route('/save').post(productController.create);
 router.route('/subscribe').post(protect, productController.subscriber);
 router.route('/findall').get(protect, productController.fetchAll);
 router.route('/findone/:id').get(protect, productController.fetchOne);
